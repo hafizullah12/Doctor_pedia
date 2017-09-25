efaa1c614991901b580271b12a5491dfbe6d2deb
hafizullah12 <uhafiz425@gmail.com>
2017-09-25 20:02:25 +0600

    Add Doctor model

---
+++ resources/views/doctors/new.blade.php
@@ -0,0 +1,25 @@
+
+<h2>Add New Doctor</h2>
+
+<div class="container">
+	<div class="row">
+	 <form action="/action_page.php">
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="email" class="form-control" placeholder="Enter email" name="name">
+    </div>
+    <div class="form-group">
+      <label for="specialist">Specialist:</label>
+      <input type="text" class="form-control" placeholder="Enter password" name="specialist">
+    </div>
+    <div class="form-group">
+      <label for="">Location:</label>
+      <input type="location" class="form-control" placeholder="Enter password" name="location">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox" name="remember"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form>
+	</div>
+</div>
